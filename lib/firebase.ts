6a3edae5faf02d275cbd6f04acdd5b67a528bea0
AkashSDas <aakashdas368@gmail.com>
2022-10-24 14:21:20 +0530

    📦 add packages and setup firebase

---
+++ lib/firebase.ts
@@ -0,0 +1,24 @@
+import { initializeApp, getApps } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+var firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FB_CONFIG_APIKEY,
+  authDomain: process.env.NEXT_PUBLIC_FB_CONFIG_AUTHDOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FB_CONFIG_PROJECTID,
+  storageBucket: process.env.NEXT_PUBLIC_FB_CONFIG_STORAGEBUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FB_CONFIG_MESSAGINGSENDERID,
+  appId: process.env.NEXT_PUBLIC_FB_CONFIG_APPID,
+  measurementId: process.env.NEXT_PUBLIC_FB_CONFIG_MEASUREMENTID,
+};
+
+// To avoid Firebase initialization error
+if (getApps().length == 0) {
+  var firebaseApp = initializeApp(firebaseConfig);
+}
+
+// SDKs for Firebase products that are to be used
+export var auth = getAuth(firebaseApp);
+export var firestore = getFirestore(firebaseApp);
+export var storage = getStorage(firebaseApp);
