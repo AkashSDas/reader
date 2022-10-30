39338842e907e7c3dc9ba3a647d51a6cad82e93f
AkashSDas <aakashdas368@gmail.com>
2022-10-30 06:46:43 +0530

    create auth check component

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,9 +1,12 @@
+import AuthCheck from "@components/auth-check";
 import { Box } from "@mui/material";
 
 function AdminPostsPage() {
   return (
     <Box>
-      <h1>Admin posts</h1>
+      <AuthCheck>
+        <h1>Admin posts</h1>
+      </AuthCheck>
     </Box>
   );
 }
